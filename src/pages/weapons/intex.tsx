e2fa70012bc0ae42c2f1b048716a0601e8e87f59
Federico <depetrisfe@gmail.com>
2022-11-26 15:31:42 +0100

    group weapons by category function

--- src/pages/weapons/intex.tsx
+++ src/pages/weapons/intex.tsx
@@ -1,15 +1,26 @@
 import { useLoaderData } from "react-router-dom";
 import { Weapon } from "../../interfaces/weapons";
 import { Grid } from "@mui/material";
+import { useStyles } from "./styles";
 
 const Weapons = () => {
+  const classes = useStyles();
   const weapons = useLoaderData() as Weapon[];
 
+  //groups weapons by category
+  const groupByCategory = () => {
+    return weapons.reduce(function (r, a) {
+      r[a.category] = r[a.category] || [];
+      r[a.category].push(a);
+      return r;
+    }, Object.create(null));
+  };
+
   return (
-    <Grid>
+    <Grid className={classes.root}>
       {weapons.map((weapon: Weapon) => (
         <Grid key={weapon.uuid}>
-          <p>{weapon.displayName}</p>
+          <img src={weapon.killStreamIcon} alt={weapon.displayName} />
         </Grid>
       ))}
     </Grid>
