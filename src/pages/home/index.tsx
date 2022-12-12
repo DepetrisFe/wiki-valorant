4be2a82c92b9d1dd915217ed2d74413dea1fcbfc
Federico <depetrisfe@gmail.com>
2022-12-12 10:25:02 +0100

    v-logo in home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,14 @@
 import { Grid } from "@mui/material";
+import { useStyles } from "./styles";
+import Vlogo from "../../assets/V-logo.png";
 
 const Home = () => {
-  return <Grid>Home page!</Grid>;
+  const classes = useStyles();
+  return (
+    <Grid className={classes.root}>
+      <img src={Vlogo} alt="logo" className={classes.logo} />
+    </Grid>
+  );
 };
 
 export default Home;
---
+++ src/pages/home/styles.tsx
@@ -0,0 +1,14 @@
+import { makeStyles } from "@mui/styles";
+
+export const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
+  logo: {
+    width: "25vw",
+    opacity: "0.2",
+  },
+});
