acb1a270e9c3d0e2325e0b1feac2e75c600941cf
Federico <depetrisfe@gmail.com>
2023-01-06 20:34:22 +0100

    background logo centered in mobile

--- src/pages/home/styles.tsx
+++ src/pages/home/styles.tsx
@@ -1,14 +1,20 @@
 import { makeStyles } from "@mui/styles";
+import { Theme } from "@mui/material";
 
-export const useStyles = makeStyles({
-  root: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100%",
-  },
-  logo: {
-    width: "25vw",
-    opacity: "0.2",
-  },
+export const useStyles = makeStyles((theme: Theme) => {
+  return {
+    root: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100%",
+      [theme.breakpoints.down("md")]: {
+        height: "80vh",
+      },
+    },
+    logo: {
+      width: "25vw",
+      opacity: "0.2",
+    },
+  };
 });
